6f530b652b29d363711b3f36c26cb195faa75da3
paulomigueljs <paulomiguelblues@gmail.com>
2022-01-06 00:52:17 -0500

    Map is working but not locating / footer added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,13 @@
 const apiKey = 'at_mThjThVI886MAVVj7geCt2R0MJw7o';
-const apiUri = 'https://geo.ipify.org/api/';
+const apiUri = 'https://geo.ipify.org/api/v1';
 
 let searchIp = document.getElementById('ip');
 let searchLocation = document.getElementById('location');
 let searchTime = document.getElementById('time');
 let searchIsp = document.getElementById('isp');
 
-const gotIp = document.getElementById('ipAddress').value;
-const searchBtn = document.getElementById('ipAddress');
+const handleInput = document.getElementById('ipAddress').value;
+const handleSubmit = document.getElementById('ipAddress');
 
 const headersOption = {
   headers: {
@@ -15,29 +15,33 @@ const headersOption = {
   },
 };
 
-const map = L.map('mapId', {
-  center: [0, 0],
-  zoom: 0,
-  layers: [
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-      attribution:
-        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-    }),
-  ],
-});
+const map = L.map('mapId').setView([51.505, -0.09], 13);
+L.tileLayer(
+  'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken:
+      'pk.eyJ1IjoibWFqZHVtbXlkZXYiLCJhIjoiY2twd2oweDl6MDUyeTJ2bjI4dWtyOXl6cSJ9.sG8ApuPSPja8lz-OsZa5DQ',
+  },
+).addTo(map);
 
-updateMarker = (updateMarker = [-20, 20]) => {
+updateMarker = (updateMarker = [40.73061, -73.935242]) => {
   map.setView(updateMarker, 13);
   L.marker(updateMarker).addTo(map);
 };
 
 getIpDetails = (defaultIp) => {
   if (defaultIp == undefined) {
-    let ipUrl = `${apiUri}?apiKey=${apiKey}`;
+    var ipUrl = `${apiUri}?apiKey=${apiKey}`;
   } else {
-    let ipUrl = `${apiUri}?apiKey=${apiKey}&ipAddress=${defaultIp}`;
+    var ipUrl = `${apiUri}?apiKey=${apiKey}&ipAddress=${defaultIp}`;
   }
-  fetch(ipUrl, headersOption)
+  fetch(ipUrl)
     .then((results) => results.json())
     .then((data) => {
       searchIp.innerHTML = data.ip;
@@ -57,8 +61,8 @@ document.addEventListener('load', updateMarker());
 
 searchBtn.addEventListener('click', (e) => {
   e.preventDefault();
-  if (gotIp.value != '' && gotIp != null) {
-    getIpDetails(gotIp.value);
+  if (handleInput.value != '' && handleInput != null) {
+    getIpDetails(handleInput.value);
 
     return;
   }
